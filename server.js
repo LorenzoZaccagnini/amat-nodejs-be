476e1525eed288a8714c1bc470d1df8de455cdac
devoleum <elisaromondia@gmail.com>
2018-12-21 22:18:21 +0100

    cors fix 2

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const app 			= express();
 const bodyParser 	= require('body-parser');
 const pg = require('pg');
 const helper 		= require('./functions/helper.js');
-const cors = require('cors')
 
-var corsOptions = {
-	origin: '*',
-	optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
-  }
-app.use(cors(corsOptions));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
