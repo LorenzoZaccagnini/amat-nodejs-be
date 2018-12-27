76246a1b07d4b1786194e359e85375d9ccfb04cb
devoleum <elisaromondia@gmail.com>
2018-12-27 11:30:33 +0100

    cors fix

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const bodyParser 	= require('body-parser');
 const pg = require('pg');
 const helper 		= require('./functions/helper.js');
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
+app.all(function(req, res, next) {
+	var origin = req.get('origin'); 
+    res.header("Access-Control-Allow-Origin", origin);
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+	res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
 app.use(bodyParser.json());
